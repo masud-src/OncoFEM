507e381a54a0024a5ae70eb6b0f91a1aacc02a19
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2023-02-20 17:50:16 +0100

    cleaning

--- oncofem/helper/auxillaries.py
+++ oncofem/helper/auxillaries.py
@@ -112,3 +112,26 @@ def norm(field: dolfin.Function):
     t.b.d.
     """
     return ufl.sqrt(sum([x*x for x in field.split()]))
+
+def calcStress_vonMises(T):
+    """
+    calculates scalar von Mises stress
+    *Arguments:*
+    T: Stress tensor (2D/3D)
+    *Example:*
+    calcStress_vonMises(T)
+    """
+    sig_x = T[0, 0]
+    sig2_x = sig_x * sig_x
+    sig_y = T[1, 1]
+    sig2_y = sig_y * sig_y
+    tau_xy = T[1, 0] * T[1, 0]
+    if ufl.shape(T)[0] == 2:
+        return ufl.sqrt(sig2_x + sig2_y - sig_x * sig_y + 3.0 * tau_xy)
+    elif ufl.shape(T)[0] == 3:
+        sig_z = T[2, 2]
+        sig2_z = sig_z * sig_z
+        tau_xz = T[0, 2] * T[0, 2]
+        tau_yz = T[1, 2] * T[1, 2]
+        return ufl.sqrt(sig2_x + sig2_y + sig2_z - sig_x * sig_y - sig_x * sig_z - sig_y * sig_z + 3.0 * (
+                    tau_xy + tau_xz + tau_yz))
