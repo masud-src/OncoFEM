9eb75b36c563896913a71d28d700f733fb9502e4
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2023-04-06 10:11:18 +0200

    added comments in the code

--- oncofem/modelling/base_model/__init__.py
+++ oncofem/modelling/base_model/__init__.py
@@ -1,1 +1,16 @@
+"""
+# **************************************************************************#
+#                                                                           #
+# === base model package  ==================================================#
+#                                                                           #
+# **************************************************************************#
+# In this sub-package of oncofem, base models can be defined.
+# 
+#
+# Author: Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
+#
+# --------------------------------------------------------------------------#
+"""
+
+from .glioblastoma import Glioblastoma
 from .stochastic_model import Stochastic_Model
