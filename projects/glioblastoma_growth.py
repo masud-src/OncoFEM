7c301c99975a27f73b80b41553d61d42dfc98df8
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2023-05-23 18:30:43 +0200

    Write new mesh...still in progress

--- projects/glioblastoma_growth.py
+++ projects/glioblastoma_growth.py
@@ -20,6 +20,57 @@ from oncofem.helper.io import set_output_file
 import oncofem.modelling.base_model.glioblastoma as bm
 from oncofem.modelling.bio_chem_models.simple_model import SimpleModel
 import academic_geometries
+import dolfin as df 
+import numpy as np
+
+
+###########################################################
+#mesh = df.UnitSquareMesh(4, 4)
+#
+#old_cells = mesh.cells()
+#old_coors = mesh.coordinates()
+#nodes_id = []
+#for i, node in enumerate(old_coors):
+#    nodes_id.append([i, node[0], node[1]])
+#
+#id = 12
+#nodes_id.pop(id)
+#
+#for i, node in enumerate(nodes_id):
+#    if node[0] > id:
+#        nodes_id[i][0] -= 1
+#
+#elements = old_cells
+#del_index = []
+#for i, element in enumerate(old_cells):
+#    if element[0] == id or element[1] == id or element[2] == id:
+#        del_index.append(i)
+#
+#elements = np.delete(elements, del_index, axis=0) 
+#
+#for i, element in enumerate(elements):
+#    for j in range(3):
+#        if element[j] > id:
+#            elements[i][j] -= 1
+#
+#new_mesh = df.Mesh() 
+#editor = df.MeshEditor() 
+#editor.open(new_mesh, mesh.ufl_cell().cellname(), mesh.geometric_dimension(), mesh.topology().dim())
+#editor.init_vertices(len(nodes_id))
+#editor.init_cells(len(elements))
+#
+#for i, node in enumerate(nodes_id):
+#    editor.add_vertex(i, np.array([node[1], node[2]]))
+#
+#for i, element in enumerate(elements):
+#    editor.add_cell(i, element)
+#
+#editor.close()
+#
+#df.plot(new_mesh)
+###########################################################
+
+
 
 # define study
 study = str.Study("paper_model")
@@ -144,3 +195,60 @@ model.set_weak_form()
 model.set_solver()
 model.set_initial_conditions(x.param.init, x.param.add)
 model.solve() 
+
+model.sol
+model.sol_old
+nSt = model.sol.sub(3)
+nSt_val = nSt.compute_vertex_values()
+idx = []
+for i, val in enumerate(nSt_val):
+    if val > 0:
+        idx.append(i)
+idx.sort(reverse=True)
+#mesh = df.UnitSquareMesh(4, 4, "left/right")
+#mesh = df.UnitSquareMesh(6, 5, "left/right")
+
+old_cells = model.mesh.cells()
+old_coors = model.mesh.coordinates()
+nodes = old_coors.tolist()
+
+ids = idx
+for id in ids:
+    nodes.pop(id)
+
+    for i, node in enumerate(nodes):
+        if node[0] > id:
+            nodes[i][0] -= 1
+
+elements = old_cells
+del_index = []
+for i, element in enumerate(old_cells):
+    for id in ids:
+        if element[0] == id or element[1] == id or element[2] == id:
+            del_index.append(i)
+
+del_index = np.unique(del_index)
+elements = np.delete(elements, del_index, axis=0) 
+
+for id in ids:
+    for i, element in enumerate(elements):
+        for j in range(3):
+            if element[j] > id:
+                elements[i][j] -= 1
+
+new_mesh = df.Mesh() 
+editor = df.MeshEditor() 
+editor.open(new_mesh, model.mesh.ufl_cell().cellname(), model.mesh.geometric_dimension(), model.mesh.topology().dim())
+editor.init_vertices(len(nodes))
+editor.init_cells(len(elements))
+
+for i, node in enumerate(nodes):
+    editor.add_vertex(i, np.array([node]))
+
+for i, element in enumerate(elements):
+    editor.add_cell(i, element)
+
+editor.close()
+
+V = df.FunctionSpace(new_mesh, "CG", 1)
+
