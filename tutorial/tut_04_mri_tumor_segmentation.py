f32a5a16bee6c7549ebd72198a5d8762709e1212
masud-src <marlon.suditsch@mechbau.uni-stuttgart.de>
2023-08-15 12:39:42 +0200

    Added prototypes in mri.py init, no need for save args anymore, need to build inference for single patient

--- tutorial/tut_04_mri_tumor_segmentation.py
+++ tutorial/tut_04_mri_tumor_segmentation.py
@@ -11,8 +11,28 @@ mri.work_dir = study.der_dir
 mri.set_tumor_segmentation()
 
 mri.tumor_segmentation.save_model_folder = mri.tumor_segmentation.ts_dir + "full_neural_net"
-mri.tumor_segmentation.model_param.training_data = "/home/marlon/Software/OncoFEM/tutorial/data/BraTS"
+mri.tumor_segmentation.model_param.arch = "EquiUnet"
+mri.tumor_segmentation.model_param.training_data = "/media/marlon/data/MRI_data/BraTS2020/TrainingData"
+mri.tumor_segmentation.model_param.full_training_data = False
 mri.tumor_segmentation.model_param.input_patterns = ["_t1", "_t1ce", "_t2", "_flair"]
+mri.tumor_segmentation.model_param.random_blank_image = False
+mri.tumor_segmentation.model_param.output_channel = 3
 mri.tumor_segmentation.model_param.width = 1
-mri.tumor_segmentation.model_param.epochs = 1
+mri.tumor_segmentation.model_param.optimizer = "ranger"
+mri.tumor_segmentation.model_param.dropout = 0.0
+mri.tumor_segmentation.model_param.warm_restart = False
+mri.tumor_segmentation.model_param.epochs = 200
+mri.tumor_segmentation.model_param.batch_size = 1
+mri.tumor_segmentation.model_param.lr = 0.0001
+mri.tumor_segmentation.model_param.weight_decay = 0.0
+mri.tumor_segmentation.model_param.no_float_precision_16 = False
+mri.tumor_segmentation.model_param.norm_layer = "group"
+mri.tumor_segmentation.model_param.n_warm_epochs = 3
+mri.tumor_segmentation.model_param.val_step_intervall = 3
+mri.tumor_segmentation.model_param.deep_sup = False
+mri.tumor_segmentation.model_param.fold = 0
+mri.tumor_segmentation.model_param.stochastic_weight_averaging = False
+mri.tumor_segmentation.model_param.stochastic_weight_averaging_repeat = 5
+mri.tumor_segmentation.model_param.workers = 2
+mri.tumor_segmentation.model_param.resume = False
 mri.tumor_segmentation.run_training()
