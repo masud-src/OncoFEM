28d392b5e1ddc4b7d12bcb9a8c73aac8efea80a3
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2023-09-05 15:12:00 +0200

    Finalized tut_02_academic_example.py for review

--- tutorial/tut_02_academic_example.py
+++ tutorial/tut_02_academic_example.py
@@ -1,77 +1,33 @@
 """
-Structure tutorial
+Academic test geometry tutorial
 
-In this second tutorial, the general structure elements of OncoFEM are shown. The user sets up a study with related 
-subjects. Also a measurement state is set up, that could be used as an input file for further investigation. For 
-simplification this example will cut at that point and shows also the possibility of OncoFEM in implementing academic 
-examples. Therefore, the central classes geometry and problem are presented. Both are necessary classes, that the gather 
-information. Finally, a simple quarter circle geometry is run with a stationary poisson equation.
+In this second tutorial, a simplified quarter circle domain shall preserve the basis for further investigations. Herein,
+again the set of governing equations read
 
-First step for performing investigations with OncoFEM in general is setting up a study. A study is initialized with a 
-title. The initialization of the study will generate a folder for all derived interim outputs and final results. The 
-base path for the studies folder can be set in config.ini file. Therefore, the first thing that should be done is 
-changing the STUDIES_DIR to the desired directory. If not already done in the installation process, please add now the 
-desired directory in config.ini. In the following first tutorial the study "tut_01" is initialized and serves as a first 
-test example.
+ 0 = div T - hatrhoF w_F
+ 0 = (nS)'_S + nS div x'_S - hatnS
+ 0 = div x'_S + div(nF w_F) - (hatrhoS / rhoSR + hatrhoF / rhoFR)
+ 0 = nF (cFt_m)'_S + div(nF cFt_m w_Ft) + cFt_m (div x'_S - hatrhoS / rhoS) - hatrhoFt / MFt_m
 
-Since in a study usually more than one subject is investigated, the user than can add several subjects by the use of the 
-"create_study" method. This is the most convenient way, because the subject already is attached to the study and
-important variables are shared.
+and are solved for the primary variables of the solid displacement u_S, the solid volume fraction nS, the fluid pressure
+pF and the tumor cell concentration cFt. Alternatively, to the first tutorial, the coupling to the solid body of the 
+micro model will be activated and a swelling can be observed.
 
-Alternatively a subject object can be created by itself and attached to the study. 
-
-To every subject in general a particular state is measured and can be taken as initial point for investigation, or time 
-point for fitting, or time point for validation. In the same manner, this can be done via a "create_state" method, that 
-is implemented in the subject class. Since a state usually corresponds to a time point this can be an argument for the 
-initialization.
-
-The time point is an optional argument, that in general is set to today. Again, a state can be created in particular and 
-connected to a subject.
-
-In order to perform patient-specific investigations, the user can add now mri measurements. Therefore, the new created 
-"measure" object is initiated via the "create_measure" Method by giving the respective path and modality.
-
-Again, this can alternatively be done via creating the measurement by itself and manually appending to the respective 
-state.
-
-To load a state into the simulation part of OncoFEM, the generated input is given to a MRI object and loaded therein. The 
-MRI object holds functionalities for performing generalisation, tumor segmentation and white matter segmentation. These 
-are documented in a separated tutorial. 
-
-The gold standard for brain tumour investigations relies on four different modalities (t1, t1ce, t2, flair). For further 
-investigations it is necessary, to check, whether the state contains all four modalities.  
-
-For simplification, OncoFEM can also handle simple academic examples, that can be created using gmsh. In this tutorial 
-folder, the file "academic_geometries.py" can be found. Herein, simple examples are already implemented and can be used 
-via the respective function. In order to use these, or the mri scans, as input geometry, the mesh and its boundaries can 
-be stored in a geometry object.
-
-A problem can be set up. It holds all necessary information for generating  initial boundary value problems. For a first 
-little example, we will set up test problem p and add the created geometry.
-
-and add related  parameters for several categories, including: general, time, material, initial or fem. In order to give 
-maximum flexibility, all categories are empty template classes and the user can fill them with arbitrary attributes. The 
-problem class also holds an attribute for the desired base model and bio-chem-model. To keep it short in this first 
-example, we will cut here and run a simple poisson problem on the generated problem. We will set the outer boundary to a 
-particular value, set up linear continuous Lagrange elements for discretisation and write the solution to an output file 
-in the problem folder. Also a production term is added, that is written in C.
-
-Next, a function for solving the poisson problem is given. This takes as anargument the generated problem and is than 
-solved in the finite element framework of FEniCS. Therefore, in this function, also the dolfin package is imported.
-
-In a last step this function is run. 
-
-Keep in mind, that with this modular programming style the function can be run with several input set-ups. These input 
-set-ups can consist in different problems, states or subjects, that are generated and can be looped.
-
-
-Author: Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
+Notes:
+A gmsh ascii file is created and meshed. This mesh is loaded and a geometry object is created. Nevertheless, again a 
+study is set up, which will set up the workspace on the hard disc. There is no need for the handling of medical image 
+data and therefore, the simple test case is set up by defining the problem and creating the geometry with the predefined 
+function 'create_Quarter_Circle'. In the following, the problem is set up by adding various parameters. Diverging from 
+the first tutorial the initial conditions are set via scalar homogeneous values and functions. The parameters of the 
+growth function need to be scaled, because of the changed problem. 
 """
 # Imports
 import oncofem as of
 import dolfin as df
 ########################################################################################################################
-# Predefined function for mesh creation
+# INPUT direct in SIMULATION CORE
+#
+# Predefined function for mesh creation.
 def create_Quarter_Circle(esize: float, fac: float, rad: float, 
                           lay: int, dfile: str, struc_mesh=True) -> of.simulation.problem.Geometry():
     """
@@ -107,25 +63,19 @@ def create_Quarter_Circle(esize: float, fac: float, rad: float,
     g.facets = facet_function
     g.dim = g.mesh.geometric_dimension()
     return g
-########################################################################################################################
-# Set up of test case
+
 study = of.Study("tut_02")
-########################################################################################################################
-# Generate geometry
 p = of.simulation.Problem()
 p.param.gen.title = "2D_QuarterCircle"
 der_file = study.der_dir + p.param.gen.title
 p.geom = create_Quarter_Circle(0.01, 1.0, 200, 60, der_file, True)  # mm
-########################################################################################################################
 # general info
 p.param.gen.flag_defSplit = True
 p.param.gen.output_file = of.helper.io.set_output_file(study.sol_dir + p.param.gen.title + "/TPM")
-########################################################################################################################
 # time parameters
 p.param.time.T_end = 1.0 * 3600 * 24.0 * 50.0  # 5 d
 p.param.time.output_interval = 1.0 * 3600 * 12.0  # 1.0 d
 p.param.time.dt = 1.0 * 3600 * 3.0  # 3 h
-########################################################################################################################
 # material parameters base model
 p.param.mat.rhoSR = 1190.0 * 1e-9  # kg / mm^3
 p.param.mat.rhoFR = 993.3 * 1e-9  # kg / mm^3
@@ -136,13 +86,11 @@ p.param.mat.lambdaS = 0.03312  # N / mm^2
 p.param.mat.muS = 0.00662  # N / mm^2
 p.param.mat.kF = 5.0e-11  # mm / s
 p.param.mat.healthy_brain_nS = 0.75
-########################################################################################################################
 # FEM parameters
 p.param.fem.solver_type = "mumps"
 p.param.fem.maxIter = 20
 p.param.fem.rel = 1E-11
 p.param.fem.abs = 1E-12
-########################################################################################################################
 # ADDITIVES material parameter
 molFt = 1.3e13  # kg / mol
 DFt = 5.0e-3  # mm^2/s
@@ -152,12 +100,10 @@ p.param.add.ele_orders = [1]
 p.param.add.tensor_orders = [0]
 p.param.add.molFkappa = [molFt]
 p.param.add.DFkappa = [DFt]
-########################################################################################################################
 # Initiate model
 model = of.simulation.base_models.TwoPhaseModel()
 model.set_param(p)
 model.set_function_spaces()
-########################################################################################################################
 # initial conditions
 p.param.init.uS_0S = [0.0, 0.0]
 p.param.init.p_0S = 0.0
@@ -166,14 +112,14 @@ field = df.Expression("c0*exp(-a*(pow((x[0]-x_s),2)+pow((x[1]-y_s),2)))",
                       degree=2, c0=1.0e-1, a=0.0008, x_s=0.0, y_s=0.0)  # mmol / l
 cFt_0S = df.interpolate(field, model.CG1_sca)
 p.param.add.cFkappa_0S = [cFt_0S]
-########################################################################################################################
 # Bio chemical set up
 bio_model = of.simulation.micro_models.VerhulstKinetic()
 bio_model.set_input(model.ansatz_functions)
 bio_model.flag_solid = True
+bio_model.speed_cFt = 2.0e7  #5.8e6  # 10e6 * mol / (m^3 s)
+bio_model.speed_nS = 2.5e-7
 prod_list = bio_model.get_output()
 model.set_micro_models(prod_list)
-########################################################################################################################
 # Boundary conditions
 bc = []
 fs = model.function_space
@@ -181,7 +127,6 @@ bc.append(df.DirichletBC(fs.sub(0).sub(1), 0.0, p.geom.facets, 2))  # 2: unstruc
 bc.append(df.DirichletBC(fs.sub(0).sub(0), 0.0, p.geom.facets, 3))  # 4: unstruc
 bc.append(df.DirichletBC(fs.sub(1), 0.0, p.geom.facets, 4))
 model.set_boundaries(bc, None)
-########################################################################################################################
 # Set up model and begin to solve
 model.set_structural_parameters()
 model.set_weak_form()
