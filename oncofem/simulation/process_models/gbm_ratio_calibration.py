ddb31cc301a292fdf8a3fe70cf7b65d8c35c99e8
marlon <marlon.suditsch@isd.uni-stuttgart.de>
2023-11-30 11:16:53 +0100

    Sigmoid function in growth process

--- oncofem/simulation/process_models/gbm_ratio_calibration.py
+++ oncofem/simulation/process_models/gbm_ratio_calibration.py
@@ -1,8 +1,8 @@
 """
 Definition of bio-chemical model set-up, that is used for simulation in model paper.
 """
-
 import dolfin as df
+import ufl
 from .process_model import ProcessModel
 
 
@@ -15,28 +15,29 @@ class GBMRatioCalibration(ProcessModel):
         self.prim_vars = None
         self.dt = None
         # healthy brain
-        self.nu_Sh_necrosis = 8.64 * 1e-05 
+        self.nu_Sh_necrosis = 4.64 * 1e-6
         self.nSn_max = 0.75
         # solid tumor
         self.nSt_ms = 8.0e-7
         self.nSt_max = 0.75
         self.nSt_init = 1.0e-2
-        self.nu_St_proliferation = 4.5856e-7  # 0.35856
+        self.nu_St_init = 4.5856e-7
+        self.nu_St_proliferation = 8.5856e-6  # 0.35856
         self.nu_St_necrosis = 1E-15 * 86400
         self.nSt_thres_lin_ms = 5e-5
         self.fac_nSt_lin_ms = 1e-1
         # mobile cancer cells
         self.cFt_ms = 7.3e-1
         self.NFt = 1E11
-        self.nu_Ft_proliferation = 8.64e7
+        self.nu_Ft_proliferation = 1.64e8
         self.nu_Ft_necrosis = 0.0 * 86400
         self.cFt_max = 9.828212E-1                  # 10e12 * mol / m^3 
         # nutrients
         self.cFn_min_growth = 0.35
         self.cFn_min_necrosis = 0.95
         self.Kgr = 0.156
-        self.nu_Fn_basal = 8.64e-12
-        self.nu_Fn_proli = 4.64e-8
+        self.nu_Fn_basal = 8.64e-10
+        self.nu_Fn_proli = 4.64e-7
 
     def set_input(self, model):
         self.prim_vars = df.split(model.ansatz_functions)
@@ -52,43 +53,39 @@ class GBMRatioCalibration(ProcessModel):
         nF = 1.0 - (nSh + nSt + nSn)
 
         hat_Sh_Fn_loss = df.Constant(0.0)  # Necrosis of healthy cells
-        hat_St_Fn_gain = df.Constant(0.0)  # Proliferation of solid tumor cells
+        hat_Ft_Fn_gain = df.Constant(0.0)
+        hat_St_Fn_gain = df.Constant(0.0)
         hat_St_Fn_loss = df.Constant(0.0)  # Necrosis of solid tumor cells
         hat_Sn_St_gain = df.Constant(0.0)  # Necrosis of solid tumor cells
-        hat_Ft_Fn_gain = df.Constant(0.0)  # Proliferation of mobile cancer cells
         hat_Ft_Fn_loss = df.Constant(0.0)  # Necrosis of mobile cancer cells
-        hat_Fn_Ft_loss = df.Constant(0.0)  # Metabolism of mobile tumor cells 
-        hat_Fn_St_loss = df.Constant(0.0)  # Metabolism of solid tumor cells 
-
-        #if self.flag_metabolism:
-        #    hat_Fn_bas_loss = self.nu_In_basal * self.molFn * self.NFt * (nF * cFt * self.molFt + nSt * self.rhoStR)
-        #    hat_Fn_pro_loss = self.f_proli * ((self.rhoStR / self.molFt) * hat_Ft_Fn_gain + hat_St_Fn_gain)
-        #    hat_cFn = - (hat_Fn_pro_loss + hat_Fn_bas_loss)
-
-        #if self.flag_necrosis:
-        #    H2 = df.conditional(df.le(cFn, self.cFn_min_necrosis), 1.0, 0.0)  # Start of Necrosis
-        #    # hat_Sh_Fn_loss = H2 * nSh * self.rhoShR * self.nu_Sh_necrosis
-        #    hat_St_Fn_loss = H2 * nSt * self.rhoStR * self.nu_St_necrosis
-        #    # hat_Ft_Fn_loss = H2 * nF * cFt * self.molFt * self.nu_Ft_necrosis
-        #    hat_Sn_Fn_gain = hat_Sh_Fn_loss + hat_St_Fn_loss + hat_Ft_Fn_loss
+        hat_Fn_Ft_loss = df.Constant(0.0)  # Metabolism of mobile tumor cells
+        hat_Fn_St_loss = df.Constant(0.0)  # Metabolism of solid tumor cells
 
         if self.flag_proliferation:
-            cond_1 = df.gt(cFn, self.cFn_min_growth)    # Enough nutrients
-            H3 = df.conditional(df.gt(cFt, self.cFt_ms), 1.0, 0.0)  # mobile cancer cells above threshold
-
-            cFt_enough_nutrients = cFt * df.Constant(self.nu_Ft_proliferation) * (1.0 - cFt / df.Constant(self.cFt_max))
-            hat_Ft_Fn_gain = df.conditional(cond_1, cFt_enough_nutrients, df.Constant(0.0))
-
-            nSt_enough_nutrients = H3 * df.Constant(self.nu_St_proliferation) * (1.0 - nSt / (self.nSt_max * 1.1))
-            hat_St_Fn_gain = df.conditional(cond_1, nSt_enough_nutrients, df.Constant(0.0))
+            cond_1 = ufl.gt(cFn, self.cFn_min_growth)    # Enough nutrients
+            cond_2 = ufl.gt(cFt, self.cFt_ms)            # mobile cancer cells above threshold
+            cond_3 = ufl.gt(cFt, self.cFt_max)           # mobile cancer cells above maximum
+            cond_4 = ufl.gt(nSt, 0.0)               # solid cancer cells are present
+            H1 = ufl.conditional(cond_1, df.Constant(1.0), df.Constant(0.0))
+            H2 = ufl.conditional(cond_2, df.Constant(1.0), df.Constant(0.0))
+            H3 = ufl.conditional(cond_3, df.Constant(0.0), df.Constant(1.0))
+            H4 = ufl.conditional(cond_4, df.Constant(0.0), df.Constant(1.0))
+            # Proliferation of mobile cancer cells
+            hat_Ft_Fn_gain = H1 * H4 * cFt * df.Constant(self.nu_Ft_proliferation) * (1.0 - cFt / df.Constant(self.cFt_max))
+            # Proliferation of solid tumor cells
+            nSt_init_sigmoid = 1.0 / (1.0 + ufl.exp(-(cFt - self.cFt_ms) / (self.cFt_max - self.cFt_ms)))
+            nSt_init = H2 * H3 * df.Constant(self.nu_St_init) * nSt_init_sigmoid
+            cond_full = self.binary_sigmoid_condition(cFt, self.cFt_ms, self.cFt_max)
+            nSt_full = cond_full * self.nu_St_proliferation * nSt * (1.0 - nSt / df.Constant(self.nSt_max))
+            hat_St_Fn_gain = H1 * (nSt_init + nSt_full)
 
         if self.flag_metabolism:
             hat_Fn_Ft_loss = cFt * df.Constant(self.nu_Fn_basal)
             hat_Fn_St_loss = nSt * df.Constant(self.nu_Fn_proli)
 
         if self.flag_necrosis:
-            cond_2 = df.lt(cFn, 1.0 - 1.0e-5)  # Enough nutrients
-            H2 = df.conditional(cond_2, 1.0, 0.0)  # Enough nutrients
+            cond_2 = ufl.lt(cFn, 1.0 - 1.0e-5)  # Enough nutrients
+            H2 = ufl.conditional(cond_2, 1.0, 0.0)  # Enough nutrients
             hat_Sh_Fn_loss = H2 * df.Constant(self.nu_Sh_necrosis) * (1.0 - cFn / (1.0 - 1.0e-5))
 
         hat_nSh = - hat_Sh_Fn_loss  
@@ -104,3 +101,17 @@ class GBMRatioCalibration(ProcessModel):
         prod_list[3] = hat_cFt  # hat_cFt
         prod_list[4] = hat_cFn  # hat_cFn
         return prod_list
+
+    def binary_sigmoid_condition(self, x, t_1, t_2, p=0.5, s=0.5):
+        """
+        Interpolates
+        """
+        c = 2.0 / (1.0-s) - 1.0
+        def f(x,p):
+            return x**c / (p**(c-1.0))
+        t_3 = p * (t_2 - t_1)
+        cond_1 = ufl.conditional(ufl.lt(x, t_1), 0.0, 1.0)
+        cond_2 = ufl.conditional(ufl.ge(x, t_1), ufl.conditional(ufl.lt(x, t_3), f(x,p), 1.0), 1.0)
+        cond_3 = ufl.conditional(ufl.ge(x, t_3), ufl.conditional(ufl.lt(x, t_2), 1-f(1-x, 1-p), 1.0), 1.0)
+
+        return cond_1 * cond_2 * cond_3
