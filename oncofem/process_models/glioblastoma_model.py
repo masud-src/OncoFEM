224cb91713f418b1906dd45f83af931a5d3dabf0
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2025-03-26 14:29:07 +0100

    refactored variables in glioblastoma_model.py

--- oncofem/process_models/glioblastoma_model.py
+++ oncofem/process_models/glioblastoma_model.py
@@ -16,15 +16,15 @@ class GlioblastomaModel(ProcessModel):
         self.cFt2nSt = self.cFt_max * 0.95                      # little less than 
         self.cFn2nSn = 0.0
         self.nSt_metabolic_switch = 1.0e-2
-        self.kappa_nSt = 9.3e-5
-        self.kappa_nSt_init = 3.1e-6
-        self.kappa_cFt = 9.3e7                                  # kappa_nSt * 10e12 * mol / m^3 
-        self.cFn_growth = 0.19e-2
-        self.cFn_basal_nSt = 1.7e0
-        self.cFn_basal_cFt = 0.0e-20
+        self.nuSt = 9.3e-5
+        self.nuSt_init = 3.1e-6
+        self.nuFt = 9.3e7                                  # kappa_nSt * 10e12 * mol / m^3 
+        self.kappa_g = 5.5e1
+        self.kappa_St_basal = 1.7e0
+        self.kappa_Ft_basal = 0.0e-20
         self.nSt_necrotic_switch = self.nSt_metabolic_switch    # 1.0e-2
-        self.kappa_nSn = self.kappa_nSt                         # 1.0e-4
-        self.kappa_nSn_init = self.kappa_nSt_init               # 1.0e-6
+        self.nuSn = self.nuSt                         # 1.0e-4
+        self.nuSn_init = self.nuSt_init               # 1.0e-6
 
     def set_input(self, model):
         self.prim_vars = df.split(model.ansatz_functions)
@@ -49,25 +49,25 @@ class GlioblastomaModel(ProcessModel):
 
         # Metabolic switch
         cond_3 = ufl.gt(nSt, self.nSt_metabolic_switch)
-        H3 = ufl.conditional(cond_3, nSt * df.Constant(self.kappa_nSt), self.kappa_nSt_init)
+        H3 = ufl.conditional(cond_3, nSt * df.Constant(self.nuSt), self.nuSt_init)
 
         # Proliferation of mobile cancer cells
-        hat_Ft_Fn_gain = cFt * df.Constant(self.kappa_cFt) * (1.0 - cFt / df.Constant(self.cFt_max))
+        hat_Ft_Fn_gain = cFt * df.Constant(self.nuFt) * (1.0 - cFt / df.Constant(self.cFt_max))
 
         # Proliferation of tumour
         hat_St_Fn_gain = H1 * (1.0 - H2) * H3 * (1.0 - nSt / df.Constant(self.nS_max))
 
         # Metabolism
-        cFn_growth = hat_St_Fn_gain * df.Constant(self.cFn_growth) * (1 - nS / self.nS_max)
-        cFn_basal_cFt = df.Constant(self.cFn_basal_cFt) * cFt * self.molFt
-        cFn_basal_nSt = df.Constant(self.cFn_basal_nSt) * nSt * self.rhoStR
+        cFn_growth = hat_St_Fn_gain * df.Constant(self.kappa_g) * (1 - nS / self.nS_max)
+        cFn_basal_cFt = df.Constant(self.kappa_Ft_basal) * cFt * self.molFt
+        cFn_basal_nSt = df.Constant(self.kappa_St_basal) * nSt * self.rhoStR
         cFn_basal = cFn_basal_nSt + cFn_basal_cFt
         hat_cFn = - (1.0 - H2) * (cFn_growth + cFn_basal)
 
         # Necrotic switch
         cond_4 = ufl.gt(nSn, self.nSt_necrotic_switch)
-        nSt_gain = nSn * df.Constant(self.kappa_nSn)
-        H4 = ufl.conditional(cond_4, nSt_gain, self.kappa_nSn_init)
+        nSt_gain = nSn * df.Constant(self.nuSn)
+        H4 = ufl.conditional(cond_4, nSt_gain, self.nuSn_init)
 
         # Necrosis
         hat_Sn_gain = H2 * H4 * (1.0 - nSn / df.Constant(self.nS_max))
@@ -77,9 +77,9 @@ class GlioblastomaModel(ProcessModel):
         H5 = ufl.conditional(cond_5, df.Constant(0.0), df.Constant(1.0))
 
         prod_list = [None] * (len(self.prim_vars) - 2)
-        prod_list[0] = - H5 * hat_St_Fn_gain                # hat_nSh              
-        prod_list[1] = H5 * hat_St_Fn_gain - hat_Sn_gain    # hat_nSt              
-        prod_list[2] = hat_Sn_gain                          # hat_nSn              
-        prod_list[3] = hat_Ft_Fn_gain                       # hat_cFt            
-        prod_list[4] = hat_cFn              
+        prod_list[0] = - H5 * hat_St_Fn_gain * self.dt / 3600                   # hat_nSh              
+        prod_list[1] = (H5 * hat_St_Fn_gain - hat_Sn_gain) * self.dt / 3600     # hat_nSt              
+        prod_list[2] = hat_Sn_gain * self.dt / 3600                             # hat_nSn              
+        prod_list[3] = hat_Ft_Fn_gain * self.dt / 3600                          # hat_cFt            
+        prod_list[4] = hat_cFn * self.dt / 3600              
         return prod_list
